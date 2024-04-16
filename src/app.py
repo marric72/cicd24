b57fee940f8945d2752fe2b954629aa5c9eb115b
Heather Marriott <heathermarriott1@gmail.com>
2024-04-16 13:00:31 -0700

    back to original to see if will deploy

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@
     some modifications by Heather M
 
     snowflake like effect from YouTube: https://youtu.be/8SLiFCnFOEo?si=kZh25qA7n4tr-Nh2
-"""
+
 from random import randint
 import streamlit as st
 from log import logger
@@ -18,7 +18,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-def ui(items: [int]) -> None:
+def ui(items: [int]) -> None: # type: ignore
 
     st.title("Heather's Streamlit Demo v0.4")
     st.subheader(".. on Azure")
@@ -32,24 +32,30 @@ def ui(items: [int]) -> None:
     # Load Animation
     animation_symbol = "🌸"
 
-    st.markdown(
-        f"""
-        <div class="snowflake">{animation_symbol}</div>
-        <div class="snowflake">{animation_symbol}</div>
-        <div class="snowflake">{animation_symbol}</div>
-        <div class="snowflake">{animation_symbol}</div>
-        <div class="snowflake">{animation_symbol}</div>
-        <div class="snowflake">{animation_symbol}</div>
-        <div class="snowflake">{animation_symbol}</div>
-        <div class="snowflake">{animation_symbol}</div>
-        <div class="snowflake">{animation_symbol}</div>
-        """,
-        unsafe_allow_html=True
-    )
+   
 
     st.line_chart(items)
     
 
+
+
+if __name__ == "__main__":
+    data = [randint(0, 10) for _ in range(25)]
+    logger.info(f"Created a list with {len(data)} items.")
+    ui(data)
+"""
+
+
+from random import randint
+import streamlit as st
+from log import logger
+
+
+def ui(items: [int]) -> None:
+
+    st.title("Streamlit Demo v0.4")
+    st.subheader(".. on Azure")
+    st.line_chart(items)
 
 
 if __name__ == "__main__":
