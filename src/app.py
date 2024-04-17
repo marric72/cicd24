eda4dc12d1d8ed5673968369724cb4bdb0b6b80e
Heather Marriott <heathermarriott1@gmail.com>
2024-04-17 08:43:03 -0700

    add newline character to end of file

--- src/app.py
+++ src/app.py
@@ -3,18 +3,18 @@
     Author: Wolf Paulus (wolf@paulus.com)
 """
 from random import randint
+from typing import List
 import streamlit as st
 from log import logger
 
 
 def ui(items: [int]) -> None:
-
     st.title("Streamlit Demo v0.4")
     st.subheader(".. on Azure")
     st.line_chart(items)
 
 
 if __name__ == "__main__":
-    data = [randint(0, 10) for _ in range(25)]
+    data: List[int] = [randint(0, 10) for _ in range(25)]
     logger.info(f"Created a list with {len(data)} items.")
     ui(data)
